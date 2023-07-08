a262947c27417203369492f7de4be8c5d8b648e1
ay-bkm <ay.belkacem@gmail.com>
2023-07-08 19:04:51 +0100

    Learning C from 0

---
+++ LearningC/day 01/firstvariable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char myName[] = "Ayoub";
+    int myAge = 27;
+    printf("Hello my name is %s and I am %d years old.", myName, myAge);
+    return 0;
+}
